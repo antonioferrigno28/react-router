ca1fbfe3be8553f991177eacd0525ca659fe9a39
antonioferrigno28 <ferrigno1@hotmail.it>
2024-12-11 14:03:42 +0100

    table done with show button

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -23,21 +23,56 @@ export default function PostsPage() {
       <div className="container py-4">
         <h1>Lista dei post:</h1>
         {posts.length > 0 ? (
-          <table>
-            <thead>
+          <table className="mt-4 table table-bordered border-dark">
+            <thead className="table-dark">
               <tr>
                 <th scope="col">Autore</th>
                 <th scope="col">Contenuto</th>
                 <th scope="col">Immagine</th>
                 <th scope="col">Categoria</th>
                 <th scope="col">Pubblico</th>
-                <th scope="col"></th>
+                <th scope="col">Cosa vuoi fare?</th>
               </tr>
             </thead>
-            <tbody></tbody>
+            <tbody>
+              {posts.map((post) => (
+                <tr key={post.id} className="table-danger">
+                  <td>
+                    <b>{post.autore}</b>
+                  </td>
+                  <td>
+                    <i>{post.contenuto}</i>
+                  </td>
+                  <td>
+                    <img src={post.immagine} width="50" />
+                  </td>
+                  <td>
+                    <span class="badge text-bg-secondary">
+                      {post.categoria}
+                    </span>
+                  </td>
+                  <td>
+                    {post.published ? (
+                      <p className="text-success fw-bold">"Pubblico"</p>
+                    ) : (
+                      <p className="text-danger fw-bold">"Privato"</p>
+                    )}
+                  </td>
+                  <td>
+                    <Link
+                      to={`/posts/${post.id}`}
+                      type="button"
+                      className="btn btn-success"
+                    >
+                      Mostra Post
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
           </table>
         ) : (
-          <p>ao</p>
+          <h1 className="mt-3 text-danger">Nessun post trovato!</h1>
         )}
       </div>
     </>
